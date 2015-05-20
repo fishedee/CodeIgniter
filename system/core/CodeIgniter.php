2f38e183910d631b5b68cf9f91397bd9ca5c1099
fishedee <306766045@qq.com>
2015-05-20 22:46:53 +0800

    加入前端定义view的能力

--- system/core/CodeIgniter.php
+++ system/core/CodeIgniter.php
@@ -552,7 +552,10 @@ if ( ! is_php('5.4'))
 			header("Cache-Control: private, no-store, no-cache, must-revalidate, max-age=0");
 			header("Cache-Control: post-check=0, pre-check=0", false);
 			header("Pragma: no-cache");
-			$CI->load->view($docComment['view'],array('data'=>$callResult));
+			if( isset($_GET['view']) )
+				$CI->load->view($_GET['view'],array('data'=>$callResult));
+			else
+				$CI->load->view($docComment['view'],array('data'=>$callResult));
 		}
 	}
 
