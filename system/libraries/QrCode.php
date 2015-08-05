a7943337567cd7bede3722b8fe11ad72ab0ad04f
Edward <milkbobo@users.noreply.github.com>
2015-08-05 09:51:07 +0800

    添加二维码接口
    添加二维码接口

--- system/libraries/QrCode.php
+++ system/libraries/QrCode.php
@@ -3,5 +3,9 @@
 		public function __construct(){
 			require_once('phpqrcode/phpqrcode.php');
 		}
+
+		public function createCodeToFile($info , $qrFileName , $errorCorrectionLevel='L',$matrixPointSize='3',$margin='5'){
+			QRcode::png($info , $qrFileName , $errorCorrectionLevel,$matrixPointSize,$margin);
+		}
 	}
  ?>
